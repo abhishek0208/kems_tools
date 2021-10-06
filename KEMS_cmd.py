412cb794ee17a1c3f89e9658a2f9b7d2b09a502e
Abhishek Shivakumar <abhishek0208@gmail.com>
2021-10-06 11:14:44 +0100

    Added timer and removed extra print commands

--- KEMS_cmd.py
+++ KEMS_cmd.py
@@ -1,14 +1,24 @@
 import os, sys
 import subprocess
 import pandas as pd
+import time
 pd.__version__
 
-input_folder = r'C:\Users\abhis\Google Drive\Kuungana Advisory_South Sudan\Model files\Model runs\14092021_v3\DAT'
-output_folder = r'C:\Users\abhis\Google Drive\Kuungana Advisory_South Sudan\Model files\Model runs\14092021_v3\JSON'
+input_folder = r'C:\Users\abhis\Google Drive\Kuungana Advisory_South Sudan\Model files\Model runs\06102021_v1\DAT'
+output_folder = r'C:\Users\abhis\Google Drive\Kuungana Advisory_South Sudan\Model files\Model runs\06102021_v1\JSON'
 model_file = r'C:\Users\abhis\Google Drive\Kuungana Advisory_South Sudan\Model files\KEMS-Dispatch_v1_3.py'
-solver = 'cbc'
+solver = 'glpk'
+
+start = time.process_time()
+file_count = 0
 
 for each_dat in os.listdir(input_folder):
+    file_count += 1
+    print('Processing ', 
+          file_count, 
+          ' out of ', 
+          len(os.listdir(input_folder)),
+          ' files')
     each_json = ('OptimisationResults_' +
                  each_dat.split('_')[1].split('.')[0] +
                  '.json')
@@ -31,6 +41,7 @@ for each_dat in os.listdir(input_folder):
                      pyomo_command, 
                      shell=True, 
                      cwd=os.getcwd()).wait()    
-    print(pyomo_command)
 
-
+print('Process took ',
+      time.process_time() - start,
+      ' seconds')
